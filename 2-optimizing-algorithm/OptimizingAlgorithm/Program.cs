65b56d4a6a6cb8be15cb5e9fdeb306f139bcd7f5
Alex Tochetto <atochetto@tech6group.com>
2024-10-15 18:02:30 -0300

    init read file and create classes

--- 2-optimizing-algorithm/OptimizingAlgorithm/Program.cs
+++ 2-optimizing-algorithm/OptimizingAlgorithm/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace OptimizingAlgorithm
 {
@@ -7,5 +9,33 @@ namespace OptimizingAlgorithm
         static void Main(string[] args)
         {
         }
+
+        private List<Voo> CarregarVoos()
+        {
+            List<Voo> voos = new List<Voo>();
+            string[] linhas = File.ReadAllLines("flights.txt");
+            foreach (string linha in linhas)
+            {
+                string[] opcoes = linha.Split(',');
+                string origem = opcoes[0];
+                string destino = opcoes[1];
+                string partida = opcoes[2];
+                string chegada = opcoes[3];
+                string preco = opcoes[4];
+            }
+        }
+    }
+
+    class Voo
+    {
+        public string Origem { get; set; }
+        public string Destino { get; set; }
+        public List<VooOpcao> VooOpcoes { get; set; }
+    }
+    class VooOpcao
+    {
+        public TimeOnly Partida { get; set; }
+        public TimeOnly Chegada { get; set; }
+        public decimal Preco { get; set; }
     }
 }
